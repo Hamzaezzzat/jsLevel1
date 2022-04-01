6c977bc4b5f17b6fdddf0603486bb0de13a2c7f5
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-04-01 19:02:46 +0200

    commit

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ const mydiv = document.querySelector(".mydiv");
 const newTitle1 = `<h1> Hello World </h1>`;
 const newTitle2 = document.createElement("h1");
 newTitle2.innerText = ("اتعلم في ساعة")
+const popup = document.querySelector(".popup");
+const buttonPopup = document.getElementById("buttonPopup");
+
+buy.addEventListener("click", (eo)=>{
+  eo.preventDefault();
+  // favDialog.showModal();
+  popup.classList.toggle("active");
+})
+buttonPopup.addEventListener("click",(eo)=>{
+  eo.preventDefault();
+  buttonPopup.toggle("active")
+})
+
+// Acting with <dialog>
+
+/*
+
+// to lecture 8
 // alot types of events
 buy.addEventListener("click",(eo)=>{
   eo.preventDefault();
@@ -16,3 +34,8 @@ buy.addEventListener("click",(eo)=>{
   mydiv.append(newTitle2);
   newTitle2.classList.add("mb");
 })
+
+buy.addEventListener("click",(eo)=>{
+  eo.target.innerText ="Hello Wolrd";
+})
+ */
