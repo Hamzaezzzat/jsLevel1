3ace3846e113e7a305489f43093afc99cab4272a
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-04-04 17:07:38 +0200

    commit

--- main.js
+++ main.js
@@ -5,25 +5,60 @@ const newTitle1 = `<h1> Hello World </h1>`;
 const newTitle2 = document.createElement("h1");
 newTitle2.innerText = ("اتعلم في ساعة")
 const popup = document.querySelector(".popup");
+const popup2 = document.querySelector(".popup2");
 const buttonPopup = document.getElementById("buttonPopup");
-const mode = document.getElementsByClassName("mode")[0];
-const dark = document.getElementsByClassName("dark")[0];
+
 
 buy.addEventListener("click", (eo)=>{
   eo.preventDefault();
   // favDialog.showModal();
   popup.classList.toggle("active");
 })
-buttonPopup.addEventListener("click",(eo)=>{
+
+setTimeout(buttonPopup.addEventListener("click",(eo) =>{
   eo.preventDefault();
-  buttonPopup.toggle("active")
-})
+  popup2.classList.toggle("active");
+}), 6000);
+
+// buttonPopup.addEventListener("click",(eo)=>{
+//   eo.preventDefault();
+//   buttonPopup.toggle("active")
+// })
 
 
 // Start Dark mode
+const mode = document.getElementsByClassName("mode")[0];
+const body = document.querySelector("body");
+
 mode.addEventListener("click",(eo)=>{
-  dark.classList.toggle("dark");
+  body.classList.toggle("dark");
 })
+
+
+/*
+// Data Types
+const data1 = "String";//String
+const data2 = 20;     // Number
+const data3 = 20.3;   // Number
+const data4 = {};     // Object
+const data5 = [];     // Number
+const data6 = ()=>{}; // function
+console.log(typeof(data1));
+*/
+
+/*
+// array
+const array = [1,2,3,4,"HTML","CSS","Javascript"];
+console.log(array[2]); // 3
+console.log(array[4]); // HTML
+console.log(array[4] += 5); // HTML5
+console.log(array[5] += 3); // CSS3
+console.log(array.length); // 7
+console.log(array.indexOf(5)); // CSS
+*/
+
+
+
 
 // Acting with <dialog>
 
